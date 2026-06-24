19c08d4126b34e9283dadb7c5a971c970d566a05
EasyLunch <bronstein300@gmail.com>
2026-06-23 21:49:00 -0300

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'burako-v15';
+const CACHE = 'burako-v16';
 const FILES = ['/', '/index.html', '/manifest.json', '/icon-192.png', '/icon-512.png'];
 
 self.addEventListener('install', e => {
